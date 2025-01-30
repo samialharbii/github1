5cd679ec94f6a85fc6c85b2a956d18a4a8babcf0
sami alharbi <sami12474@hotmail.com>
2025-01-30 13:03:19 +0300

    Revert "My panche"

--- src/main/java/ObjectsPath/HomePaths.java
+++ src/main/java/ObjectsPath/HomePaths.java
@@ -1,12 +1,4 @@
 package ObjectsPath;
 
-import org.openqa.selenium.By;
-
 public class HomePaths {
-
-    public static final By newRepBtn = By.xpath("/html/body/div[1]/div[5]/div/div/aside/div/div/loading-context/div/div[1]/div/div[1]/a/span/span[2]");
-    public static final By repName = By.xpath("//*[@id=':r5:']");
-    public static final By createRepo = By.xpath("//span[text()='Create repository']");
-    public static final By ee = By.xpath("");
-
 }
--- src/main/java/Pages/GitHubPages.java
+++ src/main/java/Pages/GitHubPages.java
@@ -1,36 +1,5 @@
 package Pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import static ObjectsPath.GitHubPaths.*;
-
 public class GitHubPages {
-    WebDriver driver = null;
-
-    public GitHubPages (WebDriver driver){
-        this.driver = driver;
-
-    }
-
-    public void openGitUrl(){
-        driver.get("https://github.com/");
-    }
-    public void clickOnSignUp(){
-        WebElement SignUpBtn = driver.findElement(signUp);
-        SignUpBtn.click();
-    }
-
-    public void clickOnTermsLink(){
-        WebElement Terms = driver.findElement(termsLink);
-        Terms.click();
-    }
-
-    public void clickOnPrivacyLink(){
-        WebElement Privacy = driver.findElement(githubPrivacy);
-        Privacy.click();
-    }
-
 
 }
-
